cc4702acf531e783fc7a31f623214472189471bd
David <temiloluwaw@gmail.com>
2020-04-28 15:31:22 +0100

    added the before and after methods

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,12 +2,32 @@ package org.example;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp(){
+        driver = new ChromeDriver();
+
+    }
+
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+
+
+
 
     }
