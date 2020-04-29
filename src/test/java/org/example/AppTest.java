7a6fb2689667d51a94edadbda2235b8771fd8e68
David <temiloluwaw@gmail.com>
2020-04-29 12:00:15 +0100

    added a selenium test to draw a house

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -10,23 +10,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import javax.swing.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
 
     private WebDriver driver;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         driver = new ChromeDriver();
 
     }
 
     @Test
-    public void seleniumExampleTest() throws InterruptedException{
+    public void seleniumExampleTest() throws InterruptedException {
         driver.manage().window().maximize();
         sleep(2000);
         driver.get("http://www.google.co.uk");
@@ -38,18 +40,20 @@ public class AppTest
         WebElement linkToPictures = driver.findElement(By.partialLinkText("Images for funny dog"));
         linkToPictures.click();
         sleep(1000);
-        WebElement imagesLink =driver.findElement(By.className("NZmxZe"));
+        WebElement imagesLink = driver.findElement(By.className("NZmxZe"));
         assertTrue(imagesLink.isDisplayed());
     }
 
     @Test
-    public void task() throws InterruptedException{
-        driver.manage().window().maximize();;
+    public void task() throws InterruptedException {
+        driver.manage().window().maximize();
         sleep(1000);
+
         driver.get("http://thedemosite.co.uk/login.php");
         WebElement addUser = driver.findElement(By.partialLinkText("Add a User"));
         addUser.click();
         sleep(1000);
+
         WebElement usernameAddUser = driver.findElement(By.name("username"));
         usernameAddUser.sendKeys("user");
         WebElement passwordAddUser = driver.findElement(By.name("password"));
@@ -61,6 +65,7 @@ public class AppTest
         WebElement login = driver.findElement(By.partialLinkText("Login"));
         login.click();
         sleep(1000);
+
         WebElement usernameLogin = driver.findElement(By.name("username"));
         usernameLogin.sendKeys("user");
         WebElement passwordLogin = driver.findElement(By.name("password"));
@@ -68,13 +73,48 @@ public class AppTest
         WebElement submitLogin = driver.findElement(By.name("FormsButton2"));
         submitLogin.click();
         sleep(1000);
+
         WebElement successfulLogin = driver.findElement(By.cssSelector("body > table > tbody > tr > td.auto-style1 > big > blockquote > blockquote > font > center > b"));
         assertTrue(successfulLogin.isDisplayed());
+        sleep(1000);
+    }
+
+    @Test
+    public void sketchTask() throws InterruptedException{
+        driver.manage().window().maximize();
+        sleep(1000);
+
+        driver.get("https://www.youidraw.com/apps/painter/");
+        Actions action = new Actions(driver);
+        action.moveByOffset(400,320).clickAndHold().moveByOffset(0,250)
+                .moveByOffset(300, 0)
+                .moveByOffset(0,-250)
+                .moveByOffset(-300,0)
+                .moveByOffset(150,-125)
+                .moveByOffset(150,125).release().perform();
+        sleep(100);
+
+        action.moveByOffset(-250,50).clickAndHold().moveByOffset(0,50)
+                .moveByOffset(50,0)
+                .moveByOffset(0,-50)
+                .moveByOffset(-50,0).release().perform();
+        sleep(1000);
+
+        action.moveByOffset(150,0).clickAndHold().moveByOffset(0,50)
+                .moveByOffset(50,0)
+                .moveByOffset(0,-50)
+                .moveByOffset(-50,0).release().perform();
+        sleep(1000);
+
+        action.moveByOffset(-90,200).clickAndHold().moveByOffset(0,-100)
+                .moveByOffset(80,0)
+                .moveByOffset(0,100).release().perform();
+        sleep(10000);
     }
 
     @After
-    public void tearDown(){
+    public void tearDown() {
         driver.close();
     }
 
-    }
+}
