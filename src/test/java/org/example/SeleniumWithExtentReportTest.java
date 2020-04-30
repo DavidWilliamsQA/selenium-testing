9d349c97d962a8bf9d3c299c1756c3b27b1323de
David <temiloluwaw@gmail.com>
2020-04-30 16:50:07 +0100

    created a method to open up the stock website, click on risers, take a screenshot and store the value for the largest riser created a method to open up the stock website, click on fallers, take a screenshot and store the value for the largest faller

--- src/test/java/org/example/SeleniumWithExtentReportTest.java
+++ src/test/java/org/example/SeleniumWithExtentReportTest.java
@@ -3,22 +3,27 @@ package org.example;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
-import org.openqa.selenium.WebDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.FileSystem;
+import java.util.Arrays;
+
+import static java.lang.Thread.*;
 
 public class SeleniumWithExtentReportTest {
 
     WebDriver driver;
     ExtentReports report;
     ExtentTest test;
+    JavascriptExecutor js;
 
     @BeforeTest
     public void startReport(){
@@ -33,10 +38,77 @@ public class SeleniumWithExtentReportTest {
     @BeforeMethod
     public void setUp(){
         driver = new ChromeDriver();
+        js = (JavascriptExecutor) driver;
     }
 
 
+    @Test
+    public void testStockMarketRiser() throws InterruptedException, IOException {
+        test = report.startTest("Checking for the largest stock price index riser");
+        driver.manage().window().maximize();
+        test.log(LogStatus.INFO, "Started chrome browser and maximised it");
+        sleep(1000);
 
+        driver.get("https://www.hl.co.uk/shares/stock-market-summary/ftse-100");
+        test.log(LogStatus.INFO, "Navigating to the FTSE 100 market overview");
+        sleep(1000);
+
+        js.executeScript("window.scrollBy(0,500)");
+
+        WebElement risersButton = driver.findElement(By.partialLinkText("RISERS"));
+        risersButton.click();
+
+        WebElement risersTable = (new WebDriverWait(driver, 5))
+                .until(ExpectedConditions.presenceOfElementLocated(By.tagName("tbody")));
+        String[] arrayRiser = risersTable.getText().split("\n");
+
+        js.executeScript("window.scrollBy(0,1000)");
+
+        File risersPage = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(risersPage, new File(System.getProperty("user.dir") + "/test-output/risersPage.jpg"));
+
+        test.log(LogStatus.PASS, "This is a screenshot showing that " + arrayRiser[0] + " is the largest riser", "<img src=\"risersPage.jpg\"/>");
+
+        test.log(LogStatus.INFO, "This is a screenshot showing that " + arrayRiser[0] + " is the largest riser");
+
+
+        sleep(10000);
+
+    }
+
+    @Test
+    public void testStockMarketFaller() throws InterruptedException, IOException {
+        test = report.startTest("Checking for the largest stock price index faller");
+        driver.manage().window().maximize();
+        test.log(LogStatus.INFO, "Started chrome browser and maximised it");
+        sleep(1000);
+
+        driver.get("https://www.hl.co.uk/shares/stock-market-summary/ftse-100");
+        test.log(LogStatus.INFO, "Navigating to the FTSE 100 market overview");
+        sleep(1000);
+
+        js.executeScript("window.scrollBy(0,500)");
+
+        WebElement risersButton = driver.findElement(By.partialLinkText("FALLERS"));
+        risersButton.click();
+
+        WebElement fallersTable = (new WebDriverWait(driver, 5))
+                .until(ExpectedConditions.presenceOfElementLocated(By.tagName("tbody")));
+        String[] arrayFaller = fallersTable.getText().split("\n");
+
+        js.executeScript("window.scrollBy(0,1000)");
+
+        File risersPage = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(risersPage, new File(System.getProperty("user.dir") + "/test-output/fallersPage.jpg"));
+
+        test.log(LogStatus.PASS, "This is a screenshot showing that " + arrayFaller[0] + " is the largest faller", "<img src=\"fallersPage.jpg\"/>");
+
+        test.log(LogStatus.INFO, "This is a screenshot showing that " + arrayFaller[0] + " is the largest faller");
+
+
+        sleep(10000);
+
+    }
 
 
 
